c24d12a45378d71a168d8e4012b04fd5c28ad044
JumpToSkyFree <alaoui.hamza031@gmail.com>
2022-12-07 21:50:32 +0100

    Add Environment variables implementation for unix
    Unix implementation of GetEnvironmentVariable, it is a function that will provide implementation to access environment variables in the unix platform.

---
+++ src/Unix/EnvironmentUnix.cc
@@ -0,0 +1,13 @@
+#include <EagleCore/Environment.hh>
+#include <string>
+
+namespace Eagle::Core {
+	std::string GetEnvironmentVariable(const char* name) {
+#if defined (_GNU_SOURCE)
+		if(const char* var = ::secure_getenv(name)) return var;
+#else
+		if(const char* var = ::getenv(name)) return var;
+#endif
+		return {};
+	}
+}
