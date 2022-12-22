80e62be9893ce95976b58ade8ad9888334d6d20e
Mohammed Faisal <44005195+noctice007@users.noreply.github.com>
2022-12-22 11:02:06 +0200

    Process Termination Executor for Win32 (#3)
    * Update EnvironmentTests.cc
    
    * Optimize Environment variable
    
    * Add maybe_unused Attributes to Function Parameters
    
    * Make Order Of Initilization Members Correct
    
    * Add Process termination executor implemenation
    
    Generic and win32 platoform implementation of process termination
    executor

--- src/Win32/ProcessTerminationImplWin32.cc
+++ src/Win32/ProcessTerminationImplWin32.cc
@@ -1,66 +1,37 @@
-/**
- * MIT License
- *
- * Copyright (c) 2022 JumpToSkyFree
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 #include "../ProcessTerminationImpl.hh"
 #include <algorithm>
-#include <memory>
-#include <sys/signal.h>
+#include <signal.h>
 
-namespace Eagle::Core {
-    struct ProcessTerminationExecutor::ProcessTerminationExecutorPlatform {
+namespace Eagle::Core{
+    struct ProcessTerminationExecutor::ProcessTerminationExecutorPlatform{
         inline static ProcessTerminationExecutorImpl* self;
-        ProcessTerminationExecutorPlatform(ProcessTerminationExecutorImpl* _self) noexcept
-        {
+        ProcessTerminationExecutorPlatform(ProcessTerminationExecutorImpl* impl){
             // Storing an instance of the private implementation as a static member, since
             // a static function that is passed to the signal function
             // do not have access to public members (this pointer is not passed) of the private implementation.
-            self = _self;
+            self = impl;
         }
-
         ProcessTerminationExecutorPlatform(const ProcessTerminationExecutorPlatform&) = delete;
         ProcessTerminationExecutorPlatform(ProcessTerminationExecutorPlatform&&) = delete;
         ProcessTerminationExecutorPlatform& operator=(const ProcessTerminationExecutorPlatform&) = delete;
         ProcessTerminationExecutorPlatform& operator=(ProcessTerminationExecutorPlatform&&) = delete;
-
-        void registerSignal() noexcept {
+        
+        void registerSignal() noexcept{
             // handle SIGTERM, SIGINT, SIGQUIT with ProcessTerminationExecutorPlatform::handleSignal
             // static function member.
             ::signal(SIGTERM, ProcessTerminationExecutorPlatform::handleSignal);
             ::signal(SIGINT, ProcessTerminationExecutorPlatform::handleSignal);
-            ::signal(SIGQUIT, ProcessTerminationExecutorPlatform::handleSignal);
+            ::signal(SIGABRT, ProcessTerminationExecutorPlatform::handleSignal);
         }
-
-        static void handleSignal(int signal) {
-            if(signal == SIGTERM or signal == SIGINT or signal == SIGQUIT) {
+        static void handleSignal(int signal){
+            if(signal == SIGTERM or signal == SIGINT or signal == SIGABRT) {
                 // Execute the termination signal handlers.
                 if(self)
                     self->executeTerminationHandlers();
             }
-
         }
-    };
 
+    };
     ProcessTerminationExecutor::ProcessTerminationExecutorImpl::ProcessTerminationExecutorImpl(bool shouldExit)
         :  _shouldExit{shouldExit}, platform{std::make_unique<ProcessTerminationExecutorPlatform>(this)}
     {
@@ -77,11 +48,11 @@ namespace Eagle::Core {
 
     void ProcessTerminationExecutor::ProcessTerminationExecutorImpl::executeTerminationHandlers() {
         std::for_each(handlers.begin(), handlers.end(), [](std::shared_ptr<IProcessTerminationTaskHandler>& handler) {
-            // If the termination task is executed successfully
+                // If the termination task is executed successfully
             if(handler->executeTerminationTask()) {
                 // execute a success callback and pass the task handler.
                 handler->terminationTaskSuccess(handler);
-                } else {
+            } else {
                 // or execute a failure callback and pass the task handler.
                 handler->terminationTaskFail(handler);
             }
